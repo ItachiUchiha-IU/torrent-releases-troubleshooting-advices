65617935908eb073dd2fb054e2398f31f1ec37ef
ItachiUchiha-IU <168943680+ItachiUchiha-IU@users.noreply.github.com>
2024-09-14 06:43:41 +0200

    Delete junk directory

--- junk/script.js
+++
@@ -1,9 +0,0 @@
-function lighttheme() {
-  var element = document.body;
-  element.classList.toggle("light");
-}
-
-function darktheme() {
-  var element = document.body;
-  element.classList.toggle("dark");
-}
